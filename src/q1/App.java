eaa96708818062627ed9004e0043ca12cbc50179
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-07-27 16:36:02 +0900

    Initial commit

---
+++ src/q1/App.java
@@ -0,0 +1,72 @@
+package q1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Student> studentList = new ArrayList<Student>();
+
+        String select = "y";
+
+        for(int i = 0; i < 10; i++){
+            Student s = new Student("학생" + (i+1), Integer.toString(i+1));
+            studentList.add(s);
+        }
+
+        System.out.print("학생 이름 입력 >> ");
+        String search = scanner.nextLine();
+        boolean is = false;
+
+        for(Student s : studentList){
+            if(s.name.equals(search)){
+                is = true;
+                System.out.println(s.name + "의 학번은 " + s.no + "입니다.");
+                break;
+            }
+        }
+
+        if (!is){
+            System.out.println("없는 학생입니다.");
+        }
+
+        while(select.equals("y")){
+            System.out.print("계속 검색 ? y 또는 n 입력 >> ");
+            is = false;
+            select = scanner.nextLine();
+
+            switch (select){
+                case "y" :{
+                    System.out.print("학생 이름 입력 >> ");
+                    search = scanner.nextLine();
+                    for (Student s : studentList){
+                        if(s.name.equals(search)){
+                            is = true;
+                            System.out.println(s.name + "의 학번은 " + s.no + "입니다.");
+                            break;
+                        }
+                    }
+                    if (!is){
+                        System.out.println("없는 학생입니다.");
+                    }
+                    break;
+                }
+                case "n" :{
+                    System.out.println("프로그램 종료");
+                    break;
+                }
+                default : {
+                    System.out.println("y 또는 n을 입력해주세요.");
+                    select = "y";
+                    continue;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ src/q1/Student.java
@@ -0,0 +1,28 @@
+package q1;
+import java.util.*;
+
+public class Student {
+    String name;
+    String no;
+
+    public Student(String name, String no) {
+        this.name = name;
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+}
