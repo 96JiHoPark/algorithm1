1186e0cfa6fc4b882432e2a097eeb5e1d2f4a355
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-08-03 19:01:11 +0900

    출력하기04

---
+++ src/CodeUp_100basic_questions/q4.java
@@ -0,0 +1,10 @@
+//https://codeup.kr/problem.php?id=1004
+//출력하기04
+
+package CodeUp_100basic_questions;
+
+public class q4 {
+    public static void main(String args[]) {
+        System.out.println("\'Hello\'");
+    }
+}
