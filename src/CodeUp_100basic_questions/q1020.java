ae16806eaec0fc8cf49ae66e3cb70c2c7a0921ba
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-08-06 23:26:29 +0900

    주민등록번호 입력 받아 형태 바꿔 출력하기

---
+++ src/CodeUp_100basic_questions/q1020.java
@@ -0,0 +1,15 @@
+//https://codeup.kr/problem.php?id=1020
+//주민번호 입력받아 형태 바꿔 출력하기
+
+package CodeUp_100basic_questions;
+
+import java.util.Scanner;
+
+public class q1020 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        s = s.replace("-","");
+        System.out.println(s);
+    }
+}
