24eec1e4ed7f79519521491c147c9bc8b9275ec2
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-08-06 23:28:18 +0900

    출력하기02

---
+++ src/CodeUp_100basic_questions/q1002.java
@@ -0,0 +1,10 @@
+//https://codeup.kr/problem.php?id=1002
+//출력하기02
+
+package CodeUp_100basic_questions;
+
+public class q1002 {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
