703c282ace4b32d24695395228acdd5b5d0e24a1
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-08-06 23:28:44 +0900

    출력하기07

---
+++ src/CodeUp_100basic_questions/q1007.java
@@ -0,0 +1,10 @@
+//https://codeup.kr/problem.php?id=1007
+//출력하기07
+
+package CodeUp_100basic_questions;
+
+public class q1007 {
+    public static void main(String args[]) {
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    }
+}
