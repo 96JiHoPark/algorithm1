fbcaf78de3594084266154e330be7d7a803e9d44
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-08-06 23:29:17 +0900

    실수 1개 입력 받아 그대로 출력하기

---
+++ src/CodeUp_100basic_questions/q1012.java
@@ -0,0 +1,14 @@
+//https://codeup.kr/problem.php?id=1012
+//실수 1개 입력받아 그대로 출력하기
+
+package CodeUp_100basic_questions;
+
+import java.util.Scanner;
+
+public class q1012 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float f = scanner.nextFloat();
+        System.out.println(String.format("%.6f",f));
+    }
+}
