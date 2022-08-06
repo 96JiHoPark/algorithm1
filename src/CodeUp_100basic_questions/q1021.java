c4a36252c0e7df3c66321f2d26b0bc362ce4a91b
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-08-06 23:26:43 +0900

    단어 1개 입력 받아 그대로 출력하기

---
+++ src/CodeUp_100basic_questions/q1021.java
@@ -0,0 +1,14 @@
+//https://codeup.kr/problem.php?id=1021
+//단어 1개 입력받아 그대로 출력하기
+
+package CodeUp_100basic_questions;
+
+import java.util.Scanner;
+
+public class q1021 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        System.out.println(s);
+    }
+}
