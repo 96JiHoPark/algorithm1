30daf4a300583b3ba2cf9e7b3b3e3b6fcddd0a9c
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-08-08 00:36:38 +0900

    정수 1개 입력받아 그대로 출력하기3

---
+++ src/CodeUp_100basic_questions/q1030.java
@@ -0,0 +1,14 @@
+//https://codeup.kr/problem.php?id=1030
+//정수 1개 입력받아 그대로 출력하기3
+
+package CodeUp_100basic_questions;
+
+import java.util.Scanner;
+
+public class q1030 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long l = scanner.nextLong();
+        System.out.println(l);
+    }
+}
